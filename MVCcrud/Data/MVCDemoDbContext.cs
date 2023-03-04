7446606fcc95be4f3efa24504c3192020254367d
Generation <Generation@DESKTOP-9PC9DBB>
2023-03-04 12:40:26 +0500

    Add project files.

---
+++ MVCcrud/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCcrud.Models.Domain;
+
+namespace MVCcrud.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
